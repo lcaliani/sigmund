ddcfb68089aad6c766ad5300290f3d971b62fa8c
Leonardo Caliani R <leu_cr@hotmail.com>
2022-02-21 14:10:58 -0300

    Adiciona novos campos de final de sessao no banco

--- database/migration(database.db).sql
+++ database/migration(database.db).sql
@@ -1,12 +1,4 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "sessao";
-CREATE TABLE IF NOT EXISTS "sessao" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"descricao"	TEXT,
-	"data_hora"	TEXT NOT NULL,
-	"id_paciente"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
 DROP TABLE IF EXISTS "roteiro_de_anamnese";
 CREATE TABLE IF NOT EXISTS "roteiro_de_anamnese" (
 	"id"	INTEGER NOT NULL UNIQUE,
@@ -22,8 +14,8 @@ CREATE TABLE IF NOT EXISTS "respostas_do_roteiro_de_anamnese" (
 	"id_paciente"	INTEGER NOT NULL,
 	"resposta"	BLOB,
 	"data_cadastro"	NUMERIC DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY("id_paciente") REFERENCES "paciente"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("id_paciente") REFERENCES "paciente"("id"),
 	FOREIGN KEY("id_roteiro") REFERENCES "roteiro_de_anamnese"("id")
 );
 DROP TABLE IF EXISTS "paciente";
@@ -39,5 +31,14 @@ CREATE TABLE IF NOT EXISTS "paciente" (
 	"status"	INTEGER NOT NULL DEFAULT 1,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-
+DROP TABLE IF EXISTS "sessao";
+CREATE TABLE IF NOT EXISTS "sessao" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"id_paciente"	INTEGER NOT NULL,
+	"descricao"	TEXT,
+	"data_hora_inicio"	TEXT NOT NULL,
+	"data_hora_fim"	TEXT NOT NULL,
+	"data_cadastro"	DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
 COMMIT;
