9e5fcbe7b84425b1e2090fc5fdf47db582351715
Leonardo Caliani R <leu_cr@hotmail.com>
2022-02-22 11:32:56 -0300

    Adiciona métodos de abrir e fechar o modal

--- src/plugins/modal/modal.js
+++ src/plugins/modal/modal.js
@@ -24,6 +24,28 @@ const setUpModal = (modalId, triggerButtonId) => {
     }
 }
 
+/**
+ * Abre o modal inicializado
+ * @param {string} triggerButtonId Nome do ID do botão que ativa o modal, sem "#"
+ * @return {undefined}
+ */
+const openModal = (triggerButtonId) => {
+    triggerButtonId = triggerButtonId || 'myBtn'
+    document.getElementById(triggerButtonId).click()
+}
+
+/**
+ * Fecha o modal inicializado
+ * @param {string} modalId Nome do modal, sem "#"
+ * @return {undefined}
+ */
+const closeModal = (modalId) => {
+    modalId = modalId || 'myModal'
+    document.querySelector(`#${modalId} .close`).click()
+}
+
 module.exports = {
     setUpModal,
+    openModal,
+    closeModal,
 }
