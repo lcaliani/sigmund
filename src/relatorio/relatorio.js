3e8662019d239698a8800ead451777ea8f65c13b
Leonardo Caliani R <leu_cr@hotmail.com>
2022-04-23 15:36:55 -0300

    Checkbox de todas as sessoẽs, marcar todas e ajustes de design

--- src/relatorio/relatorio.js
+++ src/relatorio/relatorio.js
@@ -19,7 +19,13 @@ const BOTAO_GERAR_RELATORIO = "#botao-gerar-relatorio"
 /** Botão de limpeza dos dados do relatório, exibido após retornar resultados */
 const BOTAO_LIMPAR_BUSCA = "#botao-limpar-busca"
 
-const CHECK_INCLUIR_SESSAO = '[name="incluir-no-relatorio"]:checked'
+const CHECK_INCLUIR_SESSAO_MARCADA = '[name="incluir-no-relatorio"]:checked'
+
+const CHECK_INCLUIR_SESSAO = '[name="incluir-no-relatorio"]'
+
+const CHECK_MARCAR_TODAS = '[name="marcar_todas"]'
+
+const COLUNA_NOTAS = '#coluna-notas'
 
 /**
  * Campos do formulário
@@ -28,7 +34,7 @@ const FORMULARIO = {
   ID_PACIENTE: `${ID_FORMULARIO_HTML} select[name="id_paciente"]`,
   DATA_HORA_INICIO: `${ID_FORMULARIO_HTML} input[name="data_hora_inicio"]`,
   DATA_HORA_FIM: `${ID_FORMULARIO_HTML} input[name="data_hora_fim"]`,
-  TODAS_SESSOES_CHECK : `${ID_FORMULARIO_HTML}[name="todas_as_sessoes"]`
+  TODAS_SESSOES_CHECK : `${ID_FORMULARIO_HTML} input[name="todas_as_sessoes"]`
 }
 
 /**
@@ -39,6 +45,7 @@ const MENSAGENS = {
     erro_generico: 'Houve um erro ao realizar a operação. Tente novamente.',
     notas_vazias: 'Nada consta.',
     nenhuma_sessao_escolhida: 'É necessário marcar ao menos um registro para gerar o relatório',
+    nova_busca: 'Preencha os campos e clique em Buscar para realizar uma nova busca.'
 }
 
 /**
@@ -73,7 +80,7 @@ const montarSelectHtml = async () => {
  * @param {object} registros Registros retornados do banco
  * @returns {string} html do <tbody> da tabela
  */
- function montarHtmlTabela(registros) {
+function montarHtmlTabela(registros) {
   let tableHtml = ''
   if (!registros.length) {
       tableHtml = `<tr><td colspan="6">${MENSAGENS.zero_registros}</td></tr>`
@@ -81,36 +88,41 @@ const montarSelectHtml = async () => {
       return tableHtml
   }
 
+  tableHtml += `<tr>
+    <td colspan="6" class="marcar_todas_container">
+      <input type="checkbox" name="marcar_todas" class="">
+      <small>
+        <label for="marcar_todas" class="label-inline"> Marcar todas</label>
+      </small>
+    </td>
+  </tr>`
+
   registros.forEach((registro) => {
       // Cada campo possui seu dataset
       const datasets = `data-id="${registro.id}"
         data-data_hora_inicio="${registro.data_hora_inicio}"
         data-data_hora_fim="${registro.data_hora_fim}"
-        data-id_paciente="${registro.id_paciente}"
-      `
+        data-id_paciente="${registro.id_paciente}"`
 
       const dataHoraInicio = new DateHelper(registro.data_hora_inicio)
       const dataHoraFim = new DateHelper(registro.data_hora_fim)
 
       const possuiDescricao = registro.descricao !== null
       const descricao = possuiDescricao ? registro.descricao : MENSAGENS.notas_vazias
-
+      const textColumnClass = possuiDescricao ? 'table-column-long-text-closed' : 'secondary-text'
       tableHtml += `<tr>
-          <td>${dataHoraInicio.dateBR} às <strong>${dataHoraInicio.time}</strong></td>
-          <td>${dataHoraFim.dateBR} às <strong>${dataHoraFim.time}</strong></td>
-          <td>
-            <div class="table-column-text
-            ${possuiDescricao 
-              ? 'table-column-long-text'
-              : 'secondary-text'
-            }" 
+        <td>
+          <input type="checkbox" name="incluir-no-relatorio" ${datasets}>
+        </td>
+        <td>${dataHoraInicio.dateBR} às <strong>${dataHoraInicio.time}</strong></td>
+        <td>${dataHoraFim.dateBR} às <strong>${dataHoraFim.time}</strong></td>
+        <td>
+          <div id="coluna-notas"
+            class="table-column-text ${textColumnClass}" 
             title="${descricao}">
               ${descricao}
-            </div>
-          </td>
-          <td>
-            <input type="checkbox" name="incluir-no-relatorio" ${datasets}>
-          </td>
+          </div>
+        </td>
       </tr>`
   })
 
@@ -125,15 +137,16 @@ const vincularAcoes = () => {
   document.querySelector(ID_FORMULARIO_HTML).addEventListener('submit', buscar)
 
   // Limpeza dos inputs ao cancelar edição
-  document.querySelector(`${ID_FORMULARIO_HTML} .cancel-edit`)
-    .addEventListener('click', limparCampos)
+  document.querySelector(`${ID_FORMULARIO_HTML} .cancel-edit`).addEventListener('click', limparCampos)
 
   // Limpar dados da busca na tabela
   document.querySelector(BOTAO_LIMPAR_BUSCA).addEventListener('click', limparBusca)
 
+  // Desabilitar datas de datas de sessões
+  document.querySelector(FORMULARIO.TODAS_SESSOES_CHECK).addEventListener('change', desabilitarDatas)
+
   // Gerar relatório ao clicar no botão
-  document.querySelector(BOTAO_GERAR_RELATORIO)
-    .addEventListener('click', gerarRelatorio)
+  document.querySelector(BOTAO_GERAR_RELATORIO).addEventListener('click', gerarRelatorio)
 }
 
 /**
@@ -146,6 +159,10 @@ const limparCampos = (event) => {
 
   document.querySelector(ID_FORMULARIO_HTML).reset()
   document.querySelector(`${ID_FORMULARIO_HTML} input[name="status"]`).value = 1
+  document.querySelectorAll(`${ID_FORMULARIO_HTML} input[type="date"]`)
+    .forEach((data) => {
+      data.disabled = false
+    })
 }
 
 /**
@@ -155,9 +172,14 @@ const limparCampos = (event) => {
 const buscar = async (event) => {
   event.preventDefault()
   const idPaciente = document.querySelector(`${FORMULARIO.ID_PACIENTE}`).value
-  const dataHoraInicio = `${document.querySelector(`${FORMULARIO.DATA_HORA_INICIO}`).value} 00:00`
-  const dataHoraFim = `${document.querySelector(`${FORMULARIO.DATA_HORA_FIM}`).value} 23:59`
-  // const todasAsSessoes = document.querySelector(`${FORMULARIO.TODAS_SESSOES_CHECK}`).value
+  let dataHoraInicio = null
+  let dataHoraFim = null
+
+  const buscarTodasAsSessoes = document.querySelector(FORMULARIO.TODAS_SESSOES_CHECK).checked
+  if (!buscarTodasAsSessoes) {
+    dataHoraInicio = `${document.querySelector(`${FORMULARIO.DATA_HORA_INICIO}`).value} 00:00`
+    dataHoraFim = `${document.querySelector(`${FORMULARIO.DATA_HORA_FIM}`).value} 23:59`
+  }
 
   let registros = await SessaoRepositorio.todasComPaciente(
     'data_hora_inicio',
@@ -165,12 +187,20 @@ const buscar = async (event) => {
     dataHoraInicio,
     dataHoraFim
   )
+
   document.querySelector(`[name="${NOME_TABELA_HTML}"] tbody`).innerHTML = montarHtmlTabela(registros)
-  
+
   // Disponibiliza botões para gerar o relatório
   if (registros.length > 0) {
     document.querySelector(BOTAO_GERAR_RELATORIO).classList.remove('d-none')
     document.querySelector(BOTAO_LIMPAR_BUSCA).classList.remove('d-none')
+    
+    // Exibir mais notas
+    document.querySelectorAll(COLUNA_NOTAS).forEach((nota) => {
+      nota.addEventListener('click', alternarVisibilidadeDasNotas)
+    })
+
+    document.querySelector(CHECK_MARCAR_TODAS).addEventListener('click', marcarTodasSessoes)
   }
 }
 
@@ -184,9 +214,60 @@ const limparBusca = (event) => {
     event.preventDefault();
   }
 
-  document.querySelector(`[name="${NOME_TABELA_HTML}"] tbody`).innerHTML = ''
+  document.querySelector(`[name="${NOME_TABELA_HTML}"] tbody`)
+    .innerHTML = `<tr><td colspan="6">${MENSAGENS.nova_busca}</td></tr>`
+
   document.querySelector(BOTAO_GERAR_RELATORIO).classList.add('d-none')
   document.querySelector(BOTAO_LIMPAR_BUSCA).classList.add('d-none')
+
+  limparCampos()
+}
+
+/**
+ * Limpa e desabilita os inputs de data
+ * @param {Event} event
+ * @return {undefined}
+ */
+ const desabilitarDatas = (event) => {
+  if (event !== undefined) {
+    event.preventDefault();
+  }
+
+  document.querySelectorAll(`input[type="date"]`).forEach((dateInput) => {
+    dateInput.disabled = !dateInput.disabled
+    dateInput.value = ''
+  })
+}
+
+/**
+ * Limpa e desabilita os inputs de data
+ * @param {Event} event
+ * @return {undefined}
+ */
+const alternarVisibilidadeDasNotas = (event) => {
+  if (event !== undefined) {
+    event.preventDefault();
+  }
+
+  const estaFechado = event.currentTarget.classList.contains('table-column-long-text-closed')
+  if (estaFechado) {
+    event.currentTarget.classList.remove('table-column-long-text-closed')
+    event.currentTarget.classList.add('table-column-long-text-open')
+    return
+  }
+
+  event.currentTarget.classList.remove('table-column-long-text-open')
+  event.currentTarget.classList.add('table-column-long-text-closed')
+}
+
+/**
+ * Marca todos os checkboxes para incluir sessões no relatório
+ * @param {Event} event 
+ */
+const marcarTodasSessoes = (event) => {
+  document.querySelectorAll(CHECK_INCLUIR_SESSAO).forEach((check) => {
+    check.checked = event.currentTarget.checked
+  })
 }
 
 /**
@@ -199,7 +280,7 @@ const gerarRelatorio = async (event) => {
     event.preventDefault();
   }
 
-  const sessoesEscolhidasElements = document.querySelectorAll(CHECK_INCLUIR_SESSAO)
+  const sessoesEscolhidasElements = document.querySelectorAll(CHECK_INCLUIR_SESSAO_MARCADA)
 
   if (sessoesEscolhidasElements.length === 0) {
     alert(MENSAGENS.nenhuma_sessao_escolhida)
