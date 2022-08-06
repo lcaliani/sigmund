1e2121612f2808f935b778075b1a383ae8aa26a9
Leo Caliani <leu_cr@hotmail.com>
2022-08-06 17:11:26 -0300

    Aplicação não carregando banco de dados no windows
    Corrige criação de banco através do replace de barras invertidas (#37)

--- database/conexao.js
+++ database/conexao.js
@@ -25,18 +25,20 @@ module.exports = () => {
 
 /**
  * Ao criar o build, é gerado um arquivo app.asar. O diretório em que ele se
- * encontra é somente escrita, por isso, é necessário criar uma cópia do
+ * encontra é somente leitura, por isso, é necessário criar uma cópia do
  * banco dentro de `process.env.databasePath`, que é o path correto para isso.
  * @returns {string} Caminho do arquivo de banco de dados
  */
 const prepareDatabase = () => {
     const databaseFile = path
         .join(__dirname, './database.db')
-        .replace('/app.asar', '');
+        .replace('/app.asar', '')
+        .replace('\\app.asar', ''); // Para funcionar corretamente no windows
 
     const buildDatabaseFile = path
         .join(process.env.databasePath, './database.db')
-        .replace('/app.asar', '');
+        .replace('/app.asar', '')
+        .replace('\\app.asar', ''); // Para funcionar corretamente no windows
 
     if (!existsSync(buildDatabaseFile)) {
         copyFileSync(databaseFile, buildDatabaseFile)
